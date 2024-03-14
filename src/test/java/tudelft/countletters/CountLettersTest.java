647b8b938fb109fcd33f08c601b1a90f47939eed
eveline <eveline_dejonge@hotmail.com>
2024-03-14 17:30:20 +0100

    Complete the test class for Countletter

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -17,4 +17,22 @@ public class CountLettersTest {
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void lastWordDoesNotMatch2() {
+        int words = new CountLetters().count("car");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void lastWordDoesNotMatch3() {
+        int words = new CountLetters().count("car dos");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void lastWordDoesNotMatch4() {
+        int words = new CountLetters().count("hoi ja");
+        Assertions.assertEquals(0, words);
+    }
+
 }
